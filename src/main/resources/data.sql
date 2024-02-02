08ce743c8f0a29e12546045f78cb182e6662f33b
Liplaup <paul.gardien@etud.univ-jfc.fr>
2024-02-02 08:31:13 +0100

    test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,14 @@
+
 -- Initialisation des tables
 INSERT INTO Country(id, code, name) VALUES
 -- Les clés sont auto-générées, donc on ne les spécifie pas
     (default, 'FR', 'France'), -- id = 1
     (default, 'UK', 'United Kingdom'), -- id = 2
     (default, 'US', 'United States of America'); -- id = 3
--- On peut fixer la valeur des clés auto-générées, mais il faudrait ensuite
--- réinitialiser le compteur de clé auto-générée
--- Attention : la syntaxe est différente selon le SGBD utilisé
--- ALTER TABLE Country ALTER COLUMN id RESTART WITH 4;
 
+-- attention, l'ordre des clés auto-générées n'est pas forcément séquentiel
+INSERT INTO CITY(name, population, country_id) VALUES
+    ('Paris', 12, 1),
+    ('London', 18, 2),
+    ('New York', 27, 3);
+    
